fcfa52e26d8c9ba3f853dcac828a2c29a13d3253
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-07 02:15:00 +0530

    Next pointers are of the same type in self-referential node

--- tree/src/main/java/SelfReferentialNode.java
+++ tree/src/main/java/SelfReferentialNode.java
@@ -5,25 +5,4 @@ import java.util.Objects;
  * @param <T> Type of value this node can hold
  */
 public interface SelfReferentialNode<T> extends Node<T, SelfReferentialNode<T>>
-{
-    /**
-     * Get the nth child of this self-referential data-structure
-     * @param childNumber the child to be returned
-     * @return self-referential child node
-     */
-    SelfReferentialNode<T> getChild(int childNumber);
-
-    /**
-     * As a self-referential node can have multiple children hence returning zeroth index child looks good for now
-     * @return the zeroth index child
-     */
-    default SelfReferentialNode<T> getNext()
-    {
-        return getChild(0);
-    }
-
-    default boolean hasNext()
-    {
-        return Objects.nonNull(getNext());
-    }
-}
+{}
