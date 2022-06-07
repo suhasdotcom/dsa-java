613cb2e12349cee98b890a9ba8fdc0712ec3cceb
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-07 23:26:28 +0530

    added more required method to the tree

--- tree/src/main/java/sks/dsa/tree/bst/base/tree/BinaryTree.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/tree/BinaryTree.java
@@ -2,7 +2,19 @@ package sks.dsa.tree.bst.base.tree;
 
 import sks.dsa.tree.bst.base.node.BinaryNode;
 
+/**
+ * A tree with max two children per node.
+ * @param <T> Type of data this tree nodes can store
+ */
 public interface BinaryTree<T> extends Tree<BinaryNode<T>>
 {
+    /**
+     * @return is this a balanced binary tree
+     */
+    boolean isTreeBalanced();
 
+    /**
+     * @return is thi a complete binary tree
+     */
+    boolean isTreeComplete();
 }
