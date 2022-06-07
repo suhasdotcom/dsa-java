ae70382e014b1af229b56aa5631cc3eb42a8d08c
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-08 01:04:18 +0530

    corrected constraints for TreeIterator interface

--- tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
@@ -3,8 +3,12 @@ package sks.dsa.tree.bst.base.iterator;
 import sks.dsa.tree.bst.base.node.Node;
 import sks.dsa.tree.bst.base.tree.Tree;
 
-public interface TreeIterator<T extends Tree<?>>
+import java.util.Iterator;
+
+public interface TreeIterator<NodeType extends Node<?, ? extends NodeType>,
+        TreeType extends Tree<NodeType>>
+    extends Iterator<NodeType>
 {
-    <NodeType extends Node<?, ?>> NodeType getRoot();
+    NodeType getRoot();
     int size();
 }
