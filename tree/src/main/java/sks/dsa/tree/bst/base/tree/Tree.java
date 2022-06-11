5b179691e5582841c3b9f2a3bfb7a2c752fc87ac
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-12 01:46:33 +0530

    added javadoc to Tree interface

--- tree/src/main/java/sks/dsa/tree/bst/base/tree/Tree.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/tree/Tree.java
@@ -2,13 +2,37 @@ package sks.dsa.tree.bst.base.tree;
 
 import sks.dsa.tree.bst.base.node.SelfReferentialNode;
 
+/**
+ * Interface for n-ary-tree with {@link SelfReferentialNode SelfReferentialNode} as internal node implementation
+ * @param <ValueType> the value type this tree supports
+ * @param <NodeType> a kind of {@link SelfReferentialNode SelfReferentialNode}
+ */
 public interface Tree<ValueType,
         NodeType extends SelfReferentialNode<ValueType>>
         extends Iterable<NodeType>
 {
+    /**
+     * @return this tree's root
+     */
     NodeType getRoot();
+
+    /**
+     * @return current tree's max depth
+     */
     int getMaxTreeDepth();
+
+    /**
+     * @return current tree's max height
+     */
     int getMaxTreeHeight();
+
+    /**
+     * @return current tree's number of children
+     */
     int getNumberOfChildren();
+
+    /**
+     * @return current tree's size, number of nodes in current tree
+     */
     int size();
 }
