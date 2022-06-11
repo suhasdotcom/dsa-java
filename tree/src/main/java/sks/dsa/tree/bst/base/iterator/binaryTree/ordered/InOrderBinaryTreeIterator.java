09676fa043a47913d63b90d0e786f7cfc38b2eea
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-12 00:12:23 +0530

    inOrderIteration now defined for InOrderBinaryTreeIterator

--- tree/src/main/java/sks/dsa/tree/bst/base/iterator/binaryTree/ordered/InOrderBinaryTreeIterator.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/iterator/binaryTree/ordered/InOrderBinaryTreeIterator.java
@@ -1,35 +1,67 @@
 package sks.dsa.tree.bst.base.iterator.binaryTree.ordered;
 
+import sks.dsa.tree.bst.base.iterator.binaryTree.AbstractBinaryTreeIterator;
 import sks.dsa.tree.bst.base.iterator.binaryTree.BinaryTreeIterator;
 import sks.dsa.tree.bst.base.node.binaryTree.BinaryNode;
 import sks.dsa.tree.bst.base.tree.binaryTree.BinaryTree;
 
+import java.util.Collection;
+import java.util.List;
+
 public class InOrderBinaryTreeIterator <ValueType>
+        extends AbstractBinaryTreeIterator<ValueType>
         implements BinaryTreeIterator<ValueType>
 {
+    private BinaryNode<ValueType> currentNode = getRoot();
+    private int position = 0;
 
-    @Override
-    public BinaryNode<ValueType> getRoot() {
-        return null;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public BinaryTree<ValueType> getTree() {
-        return null;
+    public InOrderBinaryTreeIterator(BinaryTree<ValueType> tree) {
+        super(tree);
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return position<size();
     }
 
     @Override
     public BinaryNode<ValueType> next() {
-        return null;
+        iterateInOrder(currentNode);
+        return currentNode;
+    }
+
+    private void iterateInOrder(BinaryNode<ValueType> theNode)
+    {
+        if(theNode == null)
+            return;
+        iterateInOrder(theNode.getLeftChild());
+        assignCurrentNodeAndIncrementCurrentPosition(theNode);
+        iterateInOrder(theNode.getRightChild());
+    }
+
+    private void assignCurrentNodeAndIncrementCurrentPosition(BinaryNode<ValueType> theNode) {
+        currentNode = theNode;
+        position++;
+    }
+
+
+    @Override
+    public void buildTreeFromSequence(final ValueType[] values) {
+
+    }
+
+    @Override
+    public void buildTreeFromSequence(BinaryNode<ValueType>[] nodes) {
+
+    }
+
+    @Override
+    public void buildTreeFromSequence(final Collection<ValueType> values) {
+
+    }
+
+    @Override
+    public void buildTreeFromSequence(List<BinaryNode<ValueType>> nodes) {
+
     }
 }
