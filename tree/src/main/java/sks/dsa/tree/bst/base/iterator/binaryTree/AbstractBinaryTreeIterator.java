332bfee2bf73ea03f667905d6784066e333223d0
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-12 00:26:56 +0530

    implemented required interface in AbstractBinaryTreeIterator

--- tree/src/main/java/sks/dsa/tree/bst/base/iterator/binaryTree/AbstractBinaryTreeIterator.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/iterator/binaryTree/AbstractBinaryTreeIterator.java
@@ -4,7 +4,7 @@ import sks.dsa.tree.bst.base.iterator.AbstractTreeIterator;
 import sks.dsa.tree.bst.base.node.binaryTree.BinaryNode;
 import sks.dsa.tree.bst.base.tree.binaryTree.BinaryTree;
 
-public abstract class AbstractBinaryTreeIterator<ValueType> extends AbstractTreeIterator<ValueType, BinaryNode<ValueType>, BinaryTree<ValueType>>
+public abstract class AbstractBinaryTreeIterator<ValueType> extends AbstractTreeIterator<ValueType, BinaryNode<ValueType>, BinaryTree<ValueType>> implements BinaryTreeIterator<ValueType>
 {
     public AbstractBinaryTreeIterator(final BinaryTree<ValueType> tree) {
         super(tree);
