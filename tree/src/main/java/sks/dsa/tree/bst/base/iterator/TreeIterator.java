35f16a268fd77751e5fecaab58c6fa6a71103239
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-12 01:24:44 +0530

    added javadoc to TreeIterator interface

--- tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
@@ -7,18 +7,53 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * The TreeIterator interface is an iterator which is to be implemented by all the iterators associated with n-ary-trees iterators
+ * @param <ValueType> The type of values this trees' nodes can store
+ * @param <NodeType> The Node Type of this tree
+ * @param <TreeType> Type of Tree
+ */
 public interface TreeIterator<ValueType,
         NodeType extends SelfReferentialNode<ValueType>,
         TreeType extends Tree<ValueType, NodeType>>
     extends Iterator<NodeType>
 {
+    /**
+     * @return the underlying trees' root node
+     */
     NodeType getRoot();
+
+    /**
+     * @return the underlying trees' size
+     */
     int size();
 
+    /**
+     * @return underlying tree
+     */
     TreeType getTree();
 
+    /**
+     * Every iterator is tasked to build the internal tree from a sequence of values in the iterators' traversal order
+     * @param values a sequence of values that will be arranged in the traversal order of this iterator
+     */
     void buildTreeFromSequence(final ValueType[] values);
+
+    /**
+     * Every iterator is tasked to build the internal tree from a sequence of values in the iterators' traversal order
+     * @param nodes a sequence of nodes that will be arranged in the traversal order of this iterator
+     */
     void buildTreeFromSequence(final NodeType [] nodes);
+
+    /**
+     * Every iterator is tasked to build the internal tree from a sequence of values in the iterators' traversal order
+     * @param values a collection of values that will be arranged in the traversal order of this iterator
+     */
     void buildTreeFromSequence(final Collection<ValueType> values);
+
+    /**
+     * Every iterator is tasked to build the internal tree from a sequence of values in the iterators' traversal order
+     * @param nodes a list of nodes that will be arranged in the traversal order of this iterator
+     */
     void buildTreeFromSequence(final List<NodeType> nodes);
 }
