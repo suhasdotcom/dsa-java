f7512c1a154a4b700653e56f95d2c64c6e469479
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-12 00:10:33 +0530

    buildTreeFromSequence added to TreeIterator

--- tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/iterator/TreeIterator.java
@@ -3,7 +3,9 @@ package sks.dsa.tree.bst.base.iterator;
 import sks.dsa.tree.bst.base.node.SelfReferentialNode;
 import sks.dsa.tree.bst.base.tree.Tree;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public interface TreeIterator<ValueType,
         NodeType extends SelfReferentialNode<ValueType>,
