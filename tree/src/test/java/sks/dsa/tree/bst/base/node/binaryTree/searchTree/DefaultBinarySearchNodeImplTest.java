30e9cc23ba59c1ca1452bf9b39c8fe07cc04c70f
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-18 22:01:31 +0530

    added test-case to DefaultBinarySearchNodeImpl

--- tree/src/test/java/sks/dsa/tree/bst/base/node/binaryTree/searchTree/DefaultBinarySearchNodeImplTest.java
+++ tree/src/test/java/sks/dsa/tree/bst/base/node/binaryTree/searchTree/DefaultBinarySearchNodeImplTest.java
@@ -16,5 +16,37 @@ class DefaultBinarySearchNodeImplTest
         assertEquals(0, rootNode.numberOfChildren());
         assertNull(rootNode.getLeftChild());
         assertNull(rootNode.getRightChild());
+        assertFalse(rootNode.hasNext());
+        assertEquals(2, rootNode.maxNumberOfNextPointers());
+    }
+
+    @Test
+    public void testForNextChildrenProperties()
+    {
+        BinarySearchNode<Integer> rootNode = new DefaultBinarySearchNodeImpl<>(4);
+        BinarySearchNode<Integer> leftNode = new DefaultBinarySearchNodeImpl<>(2);
+        BinarySearchNode<Integer> leftLeftNode = new DefaultBinarySearchNodeImpl<>(1);
+        BinarySearchNode<Integer> leftRightNode = new DefaultBinarySearchNodeImpl<>(3);
+
+        rootNode.setLeftChild(leftNode);
+        leftNode.setLeftChild(leftLeftNode);
+        leftNode.setRightChild(leftRightNode);
+
+        BinarySearchNode<Integer> rightNode = new DefaultBinarySearchNodeImpl<>(6);
+        BinarySearchNode<Integer> rightLeftNode = new DefaultBinarySearchNodeImpl<>(5);
+        BinarySearchNode<Integer> rightRightNode = new DefaultBinarySearchNodeImpl<>(7);
+
+        rootNode.setRightChild(rightNode);
+        rightNode.setLeftChild(rightLeftNode);
+        rightNode.setRightChild(rightRightNode);
+
+        assertEquals(3, rootNode.getHeight());
+        assertEquals(2, rootNode.getRightChild().getHeight());
+
+        assertEquals(0, leftLeftNode.numberOfChildren());
+        assertEquals(0, leftRightNode.numberOfChildren());
+        assertEquals(2, leftNode.numberOfChildren());
+        assertEquals(2, rightNode.numberOfChildren());
+        assertEquals(6, rootNode.numberOfChildren());
     }
 }
