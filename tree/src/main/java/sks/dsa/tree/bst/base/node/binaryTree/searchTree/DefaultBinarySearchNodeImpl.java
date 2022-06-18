11dbb0e2a85b810aadc42e0b41b49adeba873696
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-18 22:01:16 +0530

    added setters into DefaultBinarySearchNodeImpl

--- tree/src/main/java/sks/dsa/tree/bst/base/node/binaryTree/searchTree/DefaultBinarySearchNodeImpl.java
+++ tree/src/main/java/sks/dsa/tree/bst/base/node/binaryTree/searchTree/DefaultBinarySearchNodeImpl.java
@@ -1,5 +1,6 @@
 package sks.dsa.tree.bst.base.node.binaryTree.searchTree;
 
+import sks.dsa.tree.bst.base.node.SelfReferentialNode;
 import sks.dsa.tree.bst.base.node.binaryTree.BinaryNode;
 
 import java.util.List;
@@ -7,9 +8,9 @@ import java.util.NoSuchElementException;
 
 public class DefaultBinarySearchNodeImpl<ValueType extends Comparable<ValueType>> implements BinarySearchNode<ValueType> {
     private final ValueType innerValue;
-    private final BinarySearchNode<ValueType> left, right;
-    private final int height;
-    private final int numberOfChildren;
+    private BinarySearchNode<ValueType> left, right;
+    private int height;
+    private int numberOfChildren;
 
     public DefaultBinarySearchNodeImpl(final ValueType innerValue) {
         this(innerValue, null, null);
@@ -19,13 +20,20 @@ public class DefaultBinarySearchNodeImpl<ValueType extends Comparable<ValueType>
         this.innerValue = innerValue;
         this.left = left;
         this.right = right;
+        refreshHeight();
+        refreshNumberOfChildren();
+    }
+
+    private void refreshNumberOfChildren() {
+        int leftChildren = this.left!=null?this.left.numberOfChildren()+1:0;
+        int rightChildren = this.right!=null?this.right.numberOfChildren()+1:0;
+        this.numberOfChildren = leftChildren + rightChildren;
+    }
+
+    private void refreshHeight() {
         int leftHeight = this.left!=null?this.left.getHeight():0;
         int rightHeight = this.right!=null?this.right.getHeight():0;
         this.height = Math.max(leftHeight, rightHeight) + 1;
-
-        int leftChildren = this.left!=null?this.left.numberOfChildren():0;
-        int rightChildren = this.right!=null?this.right.numberOfChildren():0;
-        this.numberOfChildren = leftChildren + rightChildren;
     }
 
     @Override
@@ -56,6 +64,27 @@ public class DefaultBinarySearchNodeImpl<ValueType extends Comparable<ValueType>
     }
 
     @Override
+    public void setChild(int childNumber, final SelfReferentialNode<ValueType> nextElement) {
+        if(childNumber==0) {
+            left = (BinarySearchNode<ValueType>) nextElement;
+            refreshHeight();
+            refreshNumberOfChildren();
+        }
+        else if(childNumber==1) {
+            right = (BinarySearchNode<ValueType>) nextElement;
+            refreshHeight();
+            refreshNumberOfChildren();
+        }
+        else
+            throw new NoSuchElementException("A binary search node doesn't contain more than two children");
+    }
+
+    @Override
+    public void setNext(SelfReferentialNode<ValueType> nextElement) {
+        throw new UnsupportedOperationException("Many next pointers hence next() isn't supported on a binary-search-tree");
+    }
+
+    @Override
     public boolean hasNext() {
         return left!=null || right!=null;
     }
@@ -76,7 +105,22 @@ public class DefaultBinarySearchNodeImpl<ValueType extends Comparable<ValueType>
     }
 
     @Override
+    public void setRightChild(final BinaryNode<ValueType> nextElement) {
+        right = (BinarySearchNode<ValueType>) nextElement;
+        refreshHeight();
+        refreshNumberOfChildren();
+    }
+
+    @Override
+    public void setLeftChild(BinaryNode<ValueType> nextElement) {
+        left = (BinarySearchNode<ValueType>) nextElement;
+        refreshHeight();
+        refreshNumberOfChildren();
+    }
+
+    @Override
     public int numberOfChildren() {
+        refreshNumberOfChildren();
         return this.numberOfChildren;
     }
 
@@ -92,6 +136,7 @@ public class DefaultBinarySearchNodeImpl<ValueType extends Comparable<ValueType>
 
     @Override
     public int getHeight() {
+        refreshHeight();
         return height;
     }
 }
