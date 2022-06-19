092b96f410045b1111760a2b05e2b9eebd7e68e7
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-19 15:56:30 +0530

    added interface for BinarySearchTree

---
+++ tree/src/main/java/sks/dsa/tree/bst/base/tree/binaryTree/searchTree/BinarySearchTree.java
@@ -0,0 +1,6 @@
+package sks.dsa.tree.bst.base.tree.binaryTree.searchTree;
+
+import sks.dsa.tree.bst.base.tree.binaryTree.BinaryTree;
+
+public interface BinarySearchTree<ValueType extends Comparable<ValueType>> extends BinaryTree<ValueType> {
+}
