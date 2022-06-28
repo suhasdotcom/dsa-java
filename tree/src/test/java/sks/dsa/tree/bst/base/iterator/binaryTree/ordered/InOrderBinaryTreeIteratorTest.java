0d5376e9bb7e0ffeb726ffd7ce559189755c0b3f
Suhas Krishna Srivastava <suhasCanDoSo@gmail.com>
2022-06-28 17:52:16 +0530

    Nested test cases for InOrderBinaryTreeIteratorTest

--- tree/src/test/java/sks/dsa/tree/bst/base/iterator/binaryTree/ordered/InOrderBinaryTreeIteratorTest.java
+++ tree/src/test/java/sks/dsa/tree/bst/base/iterator/binaryTree/ordered/InOrderBinaryTreeIteratorTest.java
@@ -1,5 +1,7 @@
 package sks.dsa.tree.bst.base.iterator.binaryTree.ordered;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import sks.dsa.tree.bst.base.node.binaryTree.BinaryNode;
 import sks.dsa.tree.bst.base.node.binaryTree.searchTree.BinarySearchNode;
@@ -8,30 +10,44 @@ import sks.dsa.tree.bst.base.tree.binaryTree.searchTree.BinarySearchTree;
 import sks.dsa.tree.bst.base.tree.binaryTree.searchTree.DefaultBinarySearchTreeImpl;
 
 class InOrderBinaryTreeIteratorTest {
-    @Test
-    void testIterationOrder()
+    @Nested
+    class BinarySearchTreeTest
     {
-        BinarySearchNode<Integer> rootNode = new DefaultBinarySearchNodeImpl<>(4);
-        BinarySearchNode<Integer> leftNode = new DefaultBinarySearchNodeImpl<>(2);
-        BinarySearchNode<Integer> leftLeftNode = new DefaultBinarySearchNodeImpl<>(1);
-        BinarySearchNode<Integer> leftRightNode = new DefaultBinarySearchNodeImpl<>(3);
+        private BinarySearchTree<Integer> integerBinarySearchTree;
 
-        rootNode.setLeftChild(leftNode);
-        leftNode.setLeftChild(leftLeftNode);
-        leftNode.setRightChild(leftRightNode);
+        private BinarySearchTree<Integer> getBinarySearchTree () {
+            BinarySearchNode<Integer> rootNode = new DefaultBinarySearchNodeImpl<>(4);
+            BinarySearchNode<Integer> leftNode = new DefaultBinarySearchNodeImpl<>(2);
+            BinarySearchNode<Integer> leftLeftNode = new DefaultBinarySearchNodeImpl<>(1);
+            BinarySearchNode<Integer> leftRightNode = new DefaultBinarySearchNodeImpl<>(3);
 
-        BinarySearchNode<Integer> rightNode = new DefaultBinarySearchNodeImpl<>(6);
-        BinarySearchNode<Integer> rightLeftNode = new DefaultBinarySearchNodeImpl<>(5);
-        BinarySearchNode<Integer> rightRightNode = new DefaultBinarySearchNodeImpl<>(7);
+            rootNode.setLeftChild(leftNode);
+            leftNode.setLeftChild(leftLeftNode);
+            leftNode.setRightChild(leftRightNode);
 
-        rootNode.setRightChild(rightNode);
-        rightNode.setLeftChild(rightLeftNode);
-        rightNode.setRightChild(rightRightNode);
+            BinarySearchNode<Integer> rightNode = new DefaultBinarySearchNodeImpl<>(6);
+            BinarySearchNode<Integer> rightLeftNode = new DefaultBinarySearchNodeImpl<>(5);
+            BinarySearchNode<Integer> rightRightNode = new DefaultBinarySearchNodeImpl<>(7);
 
-        BinarySearchTree<Integer> integerBinarySearchTree = new DefaultBinarySearchTreeImpl<>(rootNode);
-        for(BinaryNode<Integer> n: integerBinarySearchTree)
-            System.out.println(n.getValue());
+            rootNode.setRightChild(rightNode);
+            rightNode.setLeftChild(rightLeftNode);
+            rightNode.setRightChild(rightRightNode);
 
-        integerBinarySearchTree.forEach(e -> System.out.println(e.getValue()));
+            return new DefaultBinarySearchTreeImpl<>(rootNode);
+        }
+        @BeforeEach
+        void setUp() {
+            integerBinarySearchTree = getBinarySearchTree();
+        }
+
+        @Test
+        void testIterationOrder ()
+        {
+            for (BinaryNode<Integer> n : integerBinarySearchTree)
+                System.out.println(n.getValue());
+
+            integerBinarySearchTree.forEach(e -> System.out.println(e.getValue()));
+        }
+
     }
 }
